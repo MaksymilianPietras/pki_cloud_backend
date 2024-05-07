ea40ec21eaa587a75db6ab29cf3803ce7ea0f645
Maksymilian <maksim2@op.pl>
2024-05-07 19:03:46 +0200

    powrot do revoke creds

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' })
 var authed = false;
 
 app.get('/', (req, res) => {
@@ -26,6 +25,7 @@ app.get('/login', (req, res) => {
         console.log(url)
         res.redirect(url);
     } else {
+        var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' })
         oauth2.userinfo.v2.me.get(function(err, result) {
           if (err){
             console.log("Niestety BLAD!!")
@@ -43,12 +43,16 @@ app.get('/login', (req, res) => {
 
 app.get('/logout', (req, res) => {
   if (authed){
-    oauth2.signOut().then(() => {
-      console.log('Użytkownik został pomyślnie wylogowany.');
-      authed = false;
-    }).catch((error) => {
-      console.error('Wystąpił błąd podczas wylogowywania:', error);
-    });    
+    oAuth2Client.revokeCredentials(function(err, response) {
+      if (err){
+        console.error("Błąd podczas wylogowywania:", err);
+      } else {
+        console.log('Użytkownik został wylogowany.');
+      }
+    });
+
+    authed = false;
+    
   } 
   res.redirect('/');
 })

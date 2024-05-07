beffc4d458d7673647b4936ffc7a8fbf754542ec
Maksymilian <maksim2@op.pl>
2024-05-07 18:57:06 +0200

    zmiana funkcji wylogowania

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const CLIENT_SECRET = OAuth2Data.web.client_secret;
 const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' })
 var authed = false;
 
 app.get('/', (req, res) => {
@@ -25,7 +26,6 @@ app.get('/login', (req, res) => {
         console.log(url)
         res.redirect(url);
     } else {
-        var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' })
         oauth2.userinfo.v2.me.get(function(err, result) {
           if (err){
             console.log("Niestety BLAD!!")
@@ -43,16 +43,12 @@ app.get('/login', (req, res) => {
 
 app.get('/logout', (req, res) => {
   if (authed){
-    oAuth2Client.revokeCredentials(function(err, response) {
-      if (err){
-        console.error("Błąd podczas wylogowywania:", err);
-      } else {
-        console.log('Użytkownik został wylogowany.');
-      }
-    });
-
-    authed = false;
-    
+    oauth2.signOut().then(() => {
+      console.log('Użytkownik został pomyślnie wylogowany.');
+      authed = false;
+    }).catch((error) => {
+      console.error('Wystąpił błąd podczas wylogowywania:', error);
+    });    
   } 
   res.redirect('/');
 })
@@ -60,7 +56,7 @@ app.get('/logout', (req, res) => {
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
     if (code) {
-        // Get an access token based on our OAuth code
+      
         oAuth2Client.getToken(code, function (err, tokens) {
             if (err) {
                 console.log('Error authenticating')

cd2a683ecd3fe23152afc6b8a12c82e2dbbcf1cd
Maksymilian <maksim2@op.pl>
2024-05-14 20:04:27 +0200

    modal poprawki

--- app.js
+++ app.js
@@ -173,27 +173,24 @@ app.listen(port, () => console.log(`Server running at ${port}`));
 
 function dbModal(err) {
   return `
-            <div class="modal" tabindex="-1" role="dialog" id="errorModal">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title">Błąd</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  <p>${err}</p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
-                </div>
-              </div>
-            </div>
+    <div class="modal fade" tabindex="-1" aria-labelledby="errorModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">${err}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Zamknij"></button>
           </div>
-            `;
+          <div class="modal-body">
+            <p>${err}</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zamknij</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 }
-
 function createUsersHtml(users) {
   let usersInHtml = `
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
